70cf0fbc831f96dfb205feb907ae3b1293cd030b
Ankitkumar Jain <ankit13jain@gmail.com>
2017-11-06 14:48:14 -0500

    Implemented XG Boost Classifier and improved accuracy for Random forest

--- PredictFirstCountry.py
+++ PredictFirstCountry.py
@@ -1,7 +1,16 @@
+import warnings
+warnings.filterwarnings("ignore", category=DeprecationWarning)
+
 import pandas as pd
 import numpy as np
 from random import randint
 from datetime import datetime
+from sklearn.preprocessing import LabelEncoder
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
+from xgboost import XGBClassifier
+from sklearn.metrics import accuracy_score
+import matplotlib.pyplot as plt
 
 def findNA(df):
     df = df.replace(r'\s+', np.nan, regex=True)
@@ -16,8 +25,11 @@ def encodeDate(df):
     return df
 
 def handle_outlier_age(df):
-    df['age']=df['age'].apply(lambda x: x-datetime.now().year if x>1900 else x)
+    df['age']=df['age'].apply(lambda x: datetime.now().year-x if x>1900 else x)
     df['age']=df['age'].apply(lambda x: x if 14<=x<=90 else np.nan)
+    mode = df['age'].mean()
+    mode = int(mode)
+    df['age']=df['age'].apply(lambda x: mode if np.isnan(x) else x) 
     return df
 
 def weightedRandomHelper(pairs):
@@ -30,16 +42,16 @@ def weightedRandomHelper(pairs):
 def weightedRandomImputation(df):
     for col in df:
         nan_count=df[col].isnull().sum()
-        print("col before ",col,nan_count)
-        print("df col size",len(df[col]))
+        #print("col before ",col,nan_count)
+        #print("df col size",len(df[col]))
         if col=='age':
             df=handle_outlier_age(df)
-        if nan_count>0 and col=='age':
+        if nan_count>0 and col!='age':
             df_counts=df[col].value_counts()
             Total_minus_unknown = 0
             Total_minus_unknown = len(df[col]) - len(df_counts)
             ratio_list=[]
-            print(df_counts[0])
+            #print(df_counts[0])
             for i in range(len(df_counts)):
                 ratio_list.append(float(df_counts[i])*100/float(Total_minus_unknown))
             min_ratio = min(ratio_list)
@@ -48,17 +60,114 @@ def weightedRandomImputation(df):
             pairs = list(zip(ratio_list,counts_list))
             df[col]=df[col].apply(lambda x: weightedRandomHelper(pairs) if(pd.isnull(x)) else x)
             nan_count=df[col].isnull().sum()
-            print("col after ",col,nan_count)
+            #print("col after ",col,nan_count)
     return df
+
+
+def randomForestDecisionClassifier(df,df_test):
+    print("\nLearning the Random Forest Classifier Model...")
+    Y_train = df.country_destination
+    X_train = df.drop('country_destination', 1)
+    X_train = X_train.drop('id', 1)
+
+    #preprocess of test
+    Y_test = df_test.country_destination
+    X_test = df_test.drop('country_destination', 1)
+    X_test = X_test.drop('id', 1)
+
+    # encode Y train
+    le = LabelEncoder()
+    Y_train = le.fit_transform(Y_train)
+
+    X_train = X_train.apply(LabelEncoder().fit_transform)
+    X_test= X_test.apply(LabelEncoder().fit_transform)
+
+    # Encode Y Test 
+    le_t = LabelEncoder()
+    Y_test = le_t.fit_transform(Y_test)
     
+    #dropping below columns as they do not improve the accuracy based on clf.feature_importances_
+    X_train = X_train.drop('language', 1)
+    X_train = X_train.drop('signup_app', 1)
+    #X_train = X_train.drop('timestamp_first_active', 1)
+    X_test = X_test.drop('language', 1)
+    X_test = X_test.drop('signup_app', 1)
+    #X_test = X_test.drop('timestamp_first_active', 1)
+
+    clf = RandomForestClassifier(max_features= 'auto', max_depth = 20, random_state=10, min_samples_split = 4, verbose =1, class_weight = 'balanced', oob_score =False, n_estimators = 100)
+
+    clf.fit(X_train, Y_train)
+    print("Importance of the features : ",clf.feature_importances_)
+    
+##    fig, ax = plt.subplots()
+##    ax.plot(list(X_train),clf.feature_importances_)
+##    plt.show()
+    Y_predict = clf.predict(X_test)
+    accuracy = clf.score(X_test, Y_test, sample_weight=None)
+    print ("Accuracy using Random Forest Classifier is : %.2f%%" % (accuracy * 100.0))
+
+def xgboostClassifier(df,df_test):
+    print("\nLearning the XGBoost Classifier Model...")
+    Y_train = df.country_destination
+    X_train = df.drop('country_destination', 1)
+    X_train = X_train.drop('id', 1)
+
+    #preprocess of test
+    Y_test = df_test.country_destination
+    X_test = df_test.drop('country_destination', 1)
+    X_test = X_test.drop('id', 1)
+
+    # encode Y train
+    le = LabelEncoder()
+    Y_train = le.fit_transform(Y_train)
+
+    X_train = X_train.apply(LabelEncoder().fit_transform)
+    X_test= X_test.apply(LabelEncoder().fit_transform)
+
+    # Encode Y Test 
+    le_t = LabelEncoder()
+    Y_test = le_t.fit_transform(Y_test)
+
+    #dropping columns as they dont improve accuracy
+    X_train = X_train.drop('timestamp_first_active', 1)
+    X_train = X_train.drop('language', 1)
+    X_train = X_train.drop('signup_app', 1)
+    X_test = X_test.drop('timestamp_first_active', 1)
+    X_test = X_test.drop('language', 1)
+    X_test = X_test.drop('signup_app', 1)
+
+
+    model = XGBClassifier()
+    model.fit(X_train, Y_train)
+    y_pred = model.predict(X_test)
+
+    predictions = [round(value) for value in y_pred]
+
+    accuracy = accuracy_score(Y_test, predictions)
+    print("Accuracy with XGBoost is : %.2f%%" % (accuracy * 100.0))
+
+
+
+###########_____Driver Code______##############
 
 df = pd.read_csv('train_users_2.csv')   #load data
+print("Doing Preprocessing")
+print("Handling Missing Values")
 df = findNA(df)
-shape=df.shape
-print(shape)
 
-dfs=np.split(df,[int(shape[0]*0.7)])
-df=dfs[0]   #Training data
-df_test=dfs[1]  #Testing data
+
+##dfs=np.split(df,[int(shape[0]*0.7)])
+##df=dfs[0]   #Training data
+##df_test=dfs[1]  #Testing data
+
+df,df_test = train_test_split( df, test_size=0.3, stratify=df['country_destination'])
+
 df=encodeDate(df)   #convert date to the day of the week with Monday=0, Sunday=6
-df=weightedRandomImputation(df)
+df=weightedRandomImputation(df) # Missing Value Imputation
+#df=binning_signup_flow(df)
+
+#preprocess of test
+df_test = encodeDate(df_test)
+df_test = weightedRandomImputation(df_test)
+randomForestDecisionClassifier(df,df_test)
+xgboostClassifier(df,df_test)
